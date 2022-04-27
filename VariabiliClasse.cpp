7b57f22f0a10da218e2fa53f6e69deeaa9b1e31b
FedericoPavone31 <fedepavone39@gmail.com>
2022-04-27 09:53:06 +0200

    Variabili Classe

--- VariabiliClasse.cpp
+++ VariabiliClasse.cpp
@@ -2,5 +2,31 @@
 using namespace std;
 
 class A {
-    
+    int a = 0;
+
+public:
+    static int b;
+
+    A() {
+        b++;
+    }
+
+    void print() {
+        cout << "a = " << a << ", b = " << b << endl;
+    }
 };
+
+int A::b = 0;
+
+int main() {
+    A a1;
+    a1.print();
+    A a2;
+    a2.print();
+    A a3;
+    a3.print();
+
+    /*a1.print();
+    a2.print();
+    a3.print();*/
+}
