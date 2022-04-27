87536563c3acc1726880529ff46b60b0a595b069
FedericoPavone31 <fedepavone39@gmail.com>
2022-04-27 09:19:27 +0200

    static inside function

--- static.cpp
+++ static.cpp
@@ -1,17 +1,23 @@
 #include<iostream>
 using namespace std;
 
-/*void fun() {
-    int a = 10;
-    cout << "non static fun a= " << a << endl;
-}*/
+void fun() {
+    int a = 0;
+    cout << "non static fun a = " << a << endl;
+    a++;
+}
 
 void fun_static() {
-    static int a = 10;
+    static int a = 0;
     cout << "static fun a = " << a << endl;
+    a++;
 }
 
 int main() {
-    fun_static();
-    cout << a << endl;
+    for(int i=0; i<10; i++) {
+        fun_static();
+    }
+    for(int j=0; j<10; j++) {
+        fun();
+    }
 }
