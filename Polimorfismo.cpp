482ddda70a6a1e57e4f82111217653c9f24edc41
FedericoPavone31 <fedepavone39@gmail.com>
2022-05-11 08:36:47 +0200

    Polimorfismo

--- Polimorfismo.cpp
+++ Polimorfismo.cpp
@@ -2,12 +2,20 @@
 using namespace std;
 
 class A {
+    private:
+        virtual void private_fun() {
+            cout << "Private function di A" << endl;
+        }
     public:
         A() {
             cout << "costruttore di A" << endl;
         }
-        void stampa() {
+        virtual void stampa() {
             cout << "Sono la classe A" << endl;
+        }
+
+        void call_private_virtual() {
+            private_fun();
         }
 };
 
@@ -19,24 +27,36 @@ class B : public A {
         void stampa() {
             cout << "Sono la classe B" << endl;
         }
+
+        void call_private_virtual() {
+            cout << "call_private_virtual di B" << endl;
+        }
 };
 
 int main() {
     A a;
     B b;
 
+    cout << "chiamata ai metodi delle istanze di A e di B" << endl;
+
     a.stampa(); //utilizza la funzione stampa di A
     b.stampa(); //utilizza la funzione stampa di B
+
+    cout << "Creo dei riferimenti di a e b, e dichiaro entrambi di classe A&" << endl;
 
     A& ra = a;
     A& rb = b;
 
-    ra.stampa(); //utilizzano entrambe la funzione stampa di A
-    rb.stampa();
+    ra.stampa(); //utilizzano entrambe la funzione stampa di A se stampa non è virtual 
+    rb.stampa(); //altrimenti il primo usa stampa di A e il secondo stampa di B
 
+    cout << "Creo dei puntatori ad A ed assegno a ciascuno l'indirizzo di a e b" << endl;
     A *pa = &a;
     A *pb = &b;
 
-    pa->stampa(); //utilizzano entrambe la funzione stampa di A
-    pb->stampa();
+    pa->stampa(); //utilizzano entrambe la funzione stampa di A se stampa non è virtual
+    pb->stampa(); //altrimenti il primo usa stampa di A e il secondo usa stampa di B
+
+    b.call_private_virtual();
+    pb->call_private_virtual();
 }
